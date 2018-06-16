d75326b30a13a72c4d1e121d27979758e2e716e4
BaneleMlamleli <mlamlelibanele@yahoo.com>
2018-06-16 02:14:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/shaun/chauma/AdminScreen.java
@@ -0,0 +1,13 @@
+package com.example.shaun.chauma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdminScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_screen);
+    }
+}
---
+++ app/src/main/java/com/example/shaun/chauma/PeerEducatorScreen.java
@@ -0,0 +1,13 @@
+package com.example.shaun.chauma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PeerEducatorScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_peer_educator_screen);
+    }
+}
