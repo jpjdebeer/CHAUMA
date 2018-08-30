68d1cc2b79bcd0f17fb5a987ed25b07d742b6724
BaneleMlamleli <mlamlelibanele@yahoo.com>
2018-08-30 23:04:08 +0200

    Jaco Up coming events change

--- app/src/main/java/com/cput/chauma/EventActivity.java
+++ app/src/main/java/com/cput/chauma/EventActivity.java
@@ -1,18 +1,36 @@
 package com.cput.chauma;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.shaun.chauma.R;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Display all the events in a calendar
@@ -26,6 +44,10 @@ public class EventActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;  //This is the  layout for the navigation bar
     private ActionBarDrawerToggle actionBarDrawerToggle; //This is the button that will be used to show and hide Navigation bar
     private Toolbar toolbar;    //This instance is for the navigation toolbar
+    FirebaseFirestore db;
+    ArrayList<Event> events;
+    LinearLayout linearLayout;
+    RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +66,7 @@ public class EventActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);    //removing the title name(in this case is was the app name)
+        db = FirebaseFirestore.getInstance();
 
         final NavigationView navigationView = findViewById(R.id.navigationViewLayout);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -75,7 +98,95 @@ public class EventActivity extends AppCompatActivity {
                 return true;
             }
         });
+        events = new ArrayList<Event>();
+        db.collection("Events")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
 
+                        if(task.getResult() == null)
+                            Toast.makeText(getApplicationContext(), "No events to show", Toast.LENGTH_SHORT).show();
+
+                        if(task.getResult() != null) {
+                            for (DocumentSnapshot document : task.getResult()) {
+                                //Log.d("Notifications", document.getId() + " => " + document.getData());
+                                ObjectMapper mapper = new ObjectMapper();
+                                Event event = mapper.convertValue(document.getData(), Event.class);
+                                events.add(event);
+                            }
+                        }
+                        ShowMessages(events);
+                    }
+                });
+
+    }
+
+    private void ShowMessages(ArrayList<Event> events) {
+        linearLayout = this.linearLayout(this);
+        relativeLayout = this.relativeLayout();
+
+        for (final Event message : events) {
+            //Log.d("Result", message.EmailFrom + " => " + message.EmailTo);
+            CardView card = new CardView(this);
+
+            Random rnd = new Random();
+            int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+
+            TextView tvEventName = this.textView(card.getContext(), message.EventName);
+            tvEventName.setTextColor(color);
+            tvEventName.setTextSize(24);
+            TextView tvEventDesc = this.textView(card.getContext(), message.EventDescription);
+            TextView tvWhen = this.textView(card.getContext(), "When: " + message.EventDate);
+
+            LinearLayout cardLinearLayout = this.linearLayout(card.getContext());
+            cardLinearLayout.addView(tvEventName);
+            cardLinearLayout.addView(tvEventDesc);
+            cardLinearLayout.addView(tvWhen);
+
+
+            card.addView(cardLinearLayout);
+            card.setContentPadding(50, 50, 50, 50);
+
+            linearLayout.addView(card);
+        }
+        ProgressBar pb = (ProgressBar) findViewById(R.id.pbLoader);
+        pb.setVisibility(View.INVISIBLE);
+        setContentView(linearLayout);
+    }
+
+    RelativeLayout relativeLayout(){
+
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+
+        //*** SET THE SIZE
+        relativeLayout.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, 100));
+
+        //*** SET BACKGROUND COLOR JUST TO MAKE LAYOUT VISIBLE
+        relativeLayout.setBackgroundColor(Color.GREEN);
+        return relativeLayout;
+    }
+
+    LinearLayout linearLayout(Context context){
+
+        LinearLayout linearLayout = new LinearLayout(context);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        return linearLayout;
+    }
+
+    TextView textView(Context context, String text){
+
+        TextView textView = new TextView(context);
+        textView.setText(text);
+        return textView;
+    }
+
+    Button button(Context context, String text){
+
+        Button button = new Button(context);
+        button.setText(text);
+        return button;
     }
 
     @Override
